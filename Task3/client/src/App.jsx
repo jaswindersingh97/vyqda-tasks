054e2012ac1b1b23ef1e72449d4ca4c9471cb83f
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-12-13 18:06:25 +0530

    commit

---
+++ Task3/client/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+function App() {
+  const onSubmit = async(e) =>{
+    e.preventDefault();
+    const response = await axios({
+      method:"post",
+      data:{
+        "phonenumber":phoneNo
+      },
+      url:"https://chimpu.online/api/post.php"
+    })
+    console.log(response);
+    setOutput({data:JSON.stringify(response.data), Headers:JSON.stringify(response.headers)});
+  }
+  const [phoneNo,setPhoneNo] = useState("");
+  const [output,setOutput] = useState({data:"",Headers:""});
+  return (
+    <>
+    <form onSubmit={onSubmit}>
+      <p>enter the phone no</p>
+      <input type="text"  placeholder='enter the number' value={phoneNo} onChange={(e)=>setPhoneNo(e.target.value)}/>
+      <button type='submit'>submit</button>      
+    </form>
+    <p>data:{output.data}</p>
+      <h1>Headers:{output.Headers}
+    </h1>
+    </>
+
+  )
+}
+
+export default App
